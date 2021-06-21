e2e38485dc0318876e32241ab57f875ea7ff17aa
Daniel Hutzley <endergeryt@gmail.com>
2021-06-20 17:09:55 -0700

    PREINIT

--- bootstrap.nix
+++ bootstrap.nix
@@ -7,7 +7,7 @@ let myNUR = builtins.getFlake "git+https://git.sr.ht/~hutzdog/NUR"; in
 pkgs.stdenv.mkDerivation {
   name = "tangle";
   nativeBuildInputs = [ lmt ];
-  src = builtins.filterSource (p: t: t != "symlink" && baseNameOf p != "out");
+  src = builtins.filterSource (p: t: t != "symlink" && dirOf p != "out");
 
   buildPhase = ''
     export PATHS=$(find ./ -type f -name '*.md')
