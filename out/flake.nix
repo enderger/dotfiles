aad9e9825ba308c57a2d21bebcfd462e2e4bcfd0
Daniel Hutzley <endergeryt@gmail.com>
2021-06-20 17:23:23 -0700

    PREINIT

---
+++ out/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My dotfiles";
+
+  inputs.nur.url = "github:nix-community/nur";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+
+  outputs = { self, nixpkgs, nur, ... }: let
+    mkPkgs = system: import nixpkgs { inherit system; overlays = [ nur.overlay ]; };
+    systems = [ "x86_64-linux" ];
+    forEachSystem = f: nixpkgs.lib.genAttrs systems f;
+  in {
+    packages = forEachSystem (system: let
+      pkgs = mkPkgs system;
+    in {
+      tangle = import ./bootstrap.nix { inherit system pkgs; lmt = pkgs.nur.repos.hutzdog.lmt; };
+    });
+  };
+}
