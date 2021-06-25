af65ac34a2d785defd086f4bac3cb080ed91de99
Daniel Hutzley <endergeryt@gmail.com>
2021-06-25 15:13:03 -0700

    PREINIT: LOTS!

--- bootstrap.nix
+++ bootstrap.nix
@@ -1,4 +1,5 @@
 # This file is used to tangle the contents of this repo into a usable form
+# Also, yes this is metaprogramming!
 let myNUR = builtins.getFlake "git+https://git.sr.ht/~hutzdog/NUR"; in
 { system ? builtins.currentSystem
 , pkgs ? import <nixpkgs> { inherit system; }
@@ -7,11 +8,11 @@ let myNUR = builtins.getFlake "git+https://git.sr.ht/~hutzdog/NUR"; in
 pkgs.stdenv.mkDerivation {
   name = "tangle";
   nativeBuildInputs = [ lmt ];
-  #src = builtins.filterSource (p: t: t != "symlink" && !(pkgs.lib.hasPrefix "." (baseNameOf p))) ./.;
   src = builtins.path { 
     filter = p: t: let
         bn = baseNameOf p;
-      in t != "symlink" && (t != "directory" || bn != "out") && !(pkgs.lib.hasPrefix "." bn);
+        inherit (pkgs) lib;
+      in t != "symlink" && (t != "directory" || bn != "out") && !(lib.hasPrefix "." bn) && !(lib.hasSuffix ".secret" bn);
     path = ./.; 
     name = "src"; 
   };
---
+++ system-composition.md
@@ -0,0 +1,8 @@
+# System Composition
+This repository uses 3 types of NixOS module to provide the ability to combine user profiles, hardware setup, and system configurations into different systems depending on the need.
+
+# Communication
+Each module will set values for the corresponding interface module.
+- The hardware module reads feature flags from the system (e.g. to set up networking if the system requires) and writes availability flags for what the system can expect to see (e.g. networking support).
+- The system, taking into account the hardware, produces a working system configuration and sets options for the user configurations to read (e.g. what window manager configs to include)
+- The users read information from the system to create user accounts tailored to it.
