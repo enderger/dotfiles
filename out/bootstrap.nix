241bdfae65119db2beeefc67bff982f2d4845f96
Daniel Hutzley <endergeryt@gmail.com>
2021-06-25 15:51:33 -0700

    PREINIT: Add license

--- out/bootstrap.nix
+++ out/bootstrap.nix
@@ -1,3 +1,9 @@
+/*
+This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+  */
+
 # This file is used to tangle the contents of this repo into a usable form
 # Also, yes this is metaprogramming!
 let myNUR = builtins.getFlake "git+https://git.sr.ht/~hutzdog/NUR"; in
